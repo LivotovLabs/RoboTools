58ace6264c2af5672787d715b52c358c094674f2
askerov <lxaskr@gmail.com>
2013-05-30 14:24:46 +0400

    intiMapForInlinePreview fixed

--- src/eu/livotov/labs/android/robotools/geo/RTGoogleMaps.java
+++ src/eu/livotov/labs/android/robotools/geo/RTGoogleMaps.java
@@ -68,7 +68,7 @@ public class RTGoogleMaps
 
     public static boolean intiMapForInlinePreview(GoogleMap map, Activity activity)
     {
-        if (activity != null && isGoogleMapsAvailable(activity))
+        if (activity != null && !isGoogleMapsAvailable(activity))
         {
             return false;
         }
