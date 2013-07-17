bb7e613faeb3224153a660ed8baae2ee38aad149
Dmitri Livotov <dmitri@livotov.eu>
2013-07-17 10:49:07 +0300

    support for loading local files and resources in async image loader

--- src/eu/livotov/labs/android/robotools/imaging/RTAsyncImageLoader.java
+++ src/eu/livotov/labs/android/robotools/imaging/RTAsyncImageLoader.java
@@ -2,6 +2,8 @@ package eu.livotov.labs.android.robotools.imaging;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v4.util.LruCache;
@@ -27,6 +29,7 @@ public class RTAsyncImageLoader
 {
 
     protected static final int DEFAULT_IMAGE_SIZE = 180;
+    private static final String DRAWABLE_STUB_SCHEMA = "drawable://";
 
     protected int defaultImageDownscaledSize = DEFAULT_IMAGE_SIZE;
     protected final ExecutorService executor;
@@ -138,6 +141,11 @@ public class RTAsyncImageLoader
         loadImage(view, url, defaultImageDownscaledSize, null);
     }
 
+    public void loadImage(ImageView view, String url, ImageLoadListener loadListener)
+    {
+        loadImage(view, url, defaultImageDownscaledSize, loadListener);
+    }
+
     public synchronized void loadImage(ImageView view, String url, int maxSize, ImageLoadListener onLoadListener)
     {
         final String tag = UUID.randomUUID().toString();
@@ -169,6 +177,26 @@ public class RTAsyncImageLoader
             request.tag = tag;
             executor.execute(new SingleImageLoadTask(request));
         }
+    }
+
+    public void loadImageFromResource(final ImageView im, final int resourceId)
+    {
+        loadImage(im, DRAWABLE_STUB_SCHEMA + resourceId);
+    }
+
+    public void loadImageFromResource(final ImageView im, final int resourceId, final ImageLoadListener listener)
+    {
+        loadImage(im, DRAWABLE_STUB_SCHEMA + resourceId, listener);
+    }
+
+    public void loadImageFromFile(final ImageView im, final File file)
+    {
+        loadImage(im, Uri.fromFile(file).toString());
+    }
+
+    public void loadImageFromFile(final ImageView im, final File file, final ImageLoadListener listener)
+    {
+        loadImage(im, Uri.fromFile(file).toString(), listener);
     }
 
     /**
@@ -215,6 +243,33 @@ public class RTAsyncImageLoader
         }
     }
 
+    /**
+     * Returns cached image immideately
+     *
+     * @return image instance as a Bitmap. If image is not chached, a null will be returned
+     */
+    public Bitmap getCachedDrawableImage(int resourceId)
+    {
+        final String key = DRAWABLE_STUB_SCHEMA + resourceId;
+
+        try
+        {
+            Bitmap bitmap = cache.get(key);
+
+            if (bitmap == null)
+            {
+                bitmap = BitmapFactory.decodeResource(context.getResources(),resourceId);
+                cache.put(key, bitmap);
+            }
+
+            return bitmap;
+        } catch (Throwable e)
+        {
+            Log.e(RTAsyncImageLoader.class.getSimpleName(), "Failed to decode resource: " + key + " > " + e.getMessage(), e);
+            return null;
+        }
+    }
+
     protected void loadPreCachedImage(ImageView view, String url, int reqSize)
     {
         Bitmap bitmap = getCachedImage(url, reqSize);
@@ -251,14 +306,17 @@ public class RTAsyncImageLoader
 
                     if (image == null)
                     {
-                        if (failoverStubResource!=0)
+                        if (failoverStubResource != 0)
                         {
-                            if (failoverImageScaleType!=null)
+                            if (failoverImageScaleType != null)
                             {
                                 request.getView().setScaleType(failoverImageScaleType);
                             }
 
                             request.getView().setImageResource(failoverStubResource);
+                        } else
+                        {
+                            request.getView().setImageResource(0);
                         }
 
                         if (request.imageLoadListener != null)
@@ -402,6 +460,7 @@ public class RTAsyncImageLoader
     {
 
         void onImageLoaded(ImageView view);
+
         void onImageFailedToLoad(final String imageUrl);
     }
 
@@ -417,33 +476,39 @@ public class RTAsyncImageLoader
 
         public void run()
         {
-            Log.d("SingleImageLoadTask", "Loading image " + request.getUrl());
+            Log.d("SingleImageLoadTask", "Loading image " + request.url);
 
             try
             {
                 if (!hasCachedImageonDisk(request.url))
                 {
-                    InputStream is = new BufferedHttpEntity(executeHttpRequest(request.getUrl())).getContent();
-                    FileOutputStream fos = new FileOutputStream(generateLocalCacheFileName(request.getUrl()));
-
-                    final byte buffer[] = new byte[8192];
-                    int read = 1;
-
-                    while (read > 0)
+                    if (request.url.startsWith(DRAWABLE_STUB_SCHEMA))
                     {
-                        read = is.read(buffer);
-                        if (read > 0)
+                        int drawable = Integer.parseInt(request.url.substring(request.url.lastIndexOf("/")+1));
+                        loadToView(request, getCachedDrawableImage(drawable));
+                    } else
+                    {
+                        InputStream is = new BufferedHttpEntity(executeHttpRequest(request.url)).getContent();
+                        FileOutputStream fos = new FileOutputStream(generateLocalCacheFileName(request.url));
+
+                        final byte buffer[] = new byte[8192];
+                        int read = 1;
+
+                        while (read > 0)
                         {
-                            fos.write(buffer, 0, read);
+                            read = is.read(buffer);
+                            if (read > 0)
+                            {
+                                fos.write(buffer, 0, read);
+                            }
                         }
+
+                        is.close();
+                        fos.flush();
+                        fos.close();
+                        loadToView(request, getCachedImage(request.url, request.reqSize));
                     }
-
-                    is.close();
-                    fos.flush();
-                    fos.close();
                 }
-
-                loadToView(request, getCachedImage(request.url, request.reqSize));
             } catch (Throwable err)
             {
                 if (request.incrementAndGetRetryCount() < 3)
