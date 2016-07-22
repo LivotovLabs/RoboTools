8d41cc6f37aa60a50149921125a198a8cfb7cc7e
grishko188cis <grishko@unrealmojo.com>
2016-07-22 11:11:19 +0300

    Code reading

--- RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/location/RTLocation.java
+++ RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/location/RTLocation.java
@@ -1,11 +1,13 @@
 package eu.livotov.labs.android.robotools.location;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+
 /**
  * Created by dlivotov on 10/07/2016.
  */
 
-public class RTLocation
-{
+public class RTLocation {
     private static final double PK = 180 / 3.1415926;
     private static final double EARTH_RADIUS = 6371;
 
@@ -18,8 +20,18 @@ public class RTLocation
      * @param lon2
      * @return Distance in km.
      */
-    public static double findDistance(double lat1, double lon1, double lat2, double lon2)
-    {
+    public static double findDistance(double lat1, double lon1, double lat2, double lon2) {
         return Math.acos(Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * (Math.cos(Math.toRadians(lon1)) * Math.cos(Math.toRadians(lon2)) + Math.sin(Math.toRadians(lon1)) * Math.sin(Math.toRadians(lon2)))) * EARTH_RADIUS;
+    }
+
+    /**
+     * * Computes distance (in meters) between two points
+     *
+     * @param fromLocation start point location
+     * @param toLocation   end point location
+     * @return distance in meters between two points provided
+     */
+    public static float findDistance(@NonNull Location fromLocation, @NonNull Location toLocation) {
+        return Math.abs(fromLocation.distanceTo(toLocation));
     }
 }
