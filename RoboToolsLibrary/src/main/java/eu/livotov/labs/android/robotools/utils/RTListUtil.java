66f315cc7f0086781c4c0a768e2a57430d6073b9
grishko188cis <grishko@unrealmojo.com>
2016-09-30 11:45:38 +0300

    RTListUtil code reading

--- RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/RTListUtil.java
+++ RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/RTListUtil.java
@@ -9,15 +9,14 @@ import java.util.List;
 /**
  * Small helper class for safe working with Lists
  */
-public class RTListUtil
-{
+public class RTListUtil {
     /**
      * Replace all elements from source into destination. Destination List will be cleared
      */
-    public static <T> void replace(@NonNull List<? super T> destination, @NonNull List<? extends T> source)
-    {
-        if (destination.size() != 0)
-        {
+    public static <T> void replace(@NonNull List<? super T> destination, @NonNull List<? extends T> source) {
+        if (destination == null || source == null)
+            return;
+        if (destination.size() != 0) {
             destination.clear();
         }
         destination.addAll(source);
@@ -26,8 +25,9 @@ public class RTListUtil
     /**
      * Add all elements from source into destination.
      */
-    public static <T> void addAll(@NonNull List<? super T> destination, @NonNull List<? extends T> source)
-    {
+    public static <T> void addAll(@NonNull List<? super T> destination, @NonNull List<? extends T> source) {
+        if (destination == null || source == null)
+            return;
         destination.addAll(source);
     }
 
@@ -35,13 +35,11 @@ public class RTListUtil
      * Add all unique elements from source into destination
      * <br/> If some element already exist in destination List, he will ignored
      */
-    public static <T> void addUnique(@NonNull List<? super T> destination, @NonNull List<? extends T> source)
-    {
-
-        for (int i = 0; i < source.size(); i++)
-        {
-            if (!destination.contains(source.get(i)))
-            {
+    public static <T> void addUnique(@NonNull List<? super T> destination, @NonNull List<? extends T> source) {
+        if (destination == null || source == null)
+            return;
+        for (int i = 0; i < source.size(); i++) {
+            if (!destination.contains(source.get(i))) {
                 destination.add(source.get(i));
             }
         }
@@ -50,10 +48,8 @@ public class RTListUtil
     /**
      * Safe check if give list contains given element
      */
-    public static <T> boolean contains(@NonNull List<? super T> list, @NonNull T element)
-    {
-        if (isEmpty(list))
-        {
+    public static <T> boolean contains(@NonNull List<? super T> list, @NonNull T element) {
+        if (isEmpty(list)) {
             return false;
         }
         return list.contains(element);
@@ -62,22 +58,17 @@ public class RTListUtil
     /**
      * Check list null or empty
      */
-    public static <T> boolean isEmpty(List<T> list)
-    {
+    public static <T> boolean isEmpty(List<T> list) {
         return list == null || list.isEmpty();
     }
 
     /**
      * Safe check if give array contains given element
      */
-    public static <T> boolean contains(@NonNull T[] array, @NonNull T element)
-    {
-        for (T item : array)
-        {
-            if (item != null)
-            {
-                if (item.equals(element))
-                {
+    public static <T> boolean contains(@NonNull T[] array, @NonNull T element) {
+        for (T item : array) {
+            if (item != null) {
+                if (item.equals(element)) {
                     return true;
                 }
             }
@@ -88,12 +79,9 @@ public class RTListUtil
     /**
      * Returns index of the give element in give array, or -1 if such element was not found
      */
-    public static <T> int indexOf(@NonNull T[] array, @NonNull T element)
-    {
-        for (int i = 0; i < array.length; i++)
-        {
-            if (array[i].equals(element))
-            {
+    public static <T> int indexOf(@NonNull T[] array, @NonNull T element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(element)) {
                 return i;
             }
         }
@@ -103,8 +91,7 @@ public class RTListUtil
     /**
      * Check if given element not last in given list
      */
-    public static <T> boolean isNotLast(@NonNull List<T> list, @NonNull T element)
-    {
+    public static <T> boolean isNotLast(@NonNull List<T> list, @NonNull T element) {
         return list.indexOf(element) < list.size() - 1;
     }
 
@@ -112,8 +99,7 @@ public class RTListUtil
      * Check if such position safe for given list to get some element.
      * <br/> Use to avoid {@link ArrayIndexOutOfBoundsException} or etc.
      */
-    public static <T> boolean isSafePosittion(List<T> list, int position)
-    {
+    public static <T> boolean isSafePosition(List<T> list, int position) {
         return !isEmpty(list) && isSafePosition(list.size(), position);
     }
 
@@ -121,26 +107,22 @@ public class RTListUtil
      * Check if such position safe for given list size to get some element.
      * <br/> Use to avoid {@link ArrayIndexOutOfBoundsException} or etc.
      */
-    public static boolean isSafePosition(int totalCount, int position)
-    {
+    public static boolean isSafePosition(int totalCount, int position) {
         return position >= 0 && position < totalCount;
     }
 
     /**
      * Returns last element in list, or null if list isEmpty
      */
-    public static <T> T getLast(@NonNull List<T> list)
-    {
+    public static <T> T getLast(@NonNull List<T> list) {
         return isEmpty(list) ? null : list.get(list.size() - 1);
     }
 
     /**
      * Safe wrapper method for {@link Collections#sort(List)}
      */
-    public static <T extends Comparable> void sort(@NonNull List<T> list)
-    {
-        if (isEmpty(list))
-        {
+    public static <T extends Comparable<T>> void sort(@NonNull List<T> list) {
+        if (isEmpty(list)) {
             return;
         }
         Collections.sort(list);
@@ -149,12 +131,17 @@ public class RTListUtil
     /**
      * Safe wrapper method for {@link Collections#sort(List, Comparator)}
      */
-    public static <T extends Comparable> void sort(@NonNull List<T> list, Comparator<? super T> comparator)
-    {
-        if (isEmpty(list) || comparator == null)
-        {
+    public static <T> void sort(@NonNull List<T> list, Comparator<? super T> comparator) {
+        if (isEmpty(list) || comparator == null) {
             return;
         }
         Collections.sort(list, comparator);
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(@NonNull List<T> list, T key) {
+        if (isEmpty(list)) {
+            return -1;
+        }
+        return Collections.binarySearch(list, key);
     }
 }
