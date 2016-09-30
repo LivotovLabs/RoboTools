0df531cf32ea98249b5262068b68768605b6fe0d
grishko188cis <grishko@unrealmojo.com>
2016-09-30 11:36:00 +0300

    Code reading

--- RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/banking/RTCreditCard.java
+++ RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/banking/RTCreditCard.java
@@ -5,10 +5,8 @@ import android.text.TextUtils;
 /**
  * Various credit-card data handling methods
  */
-public class RTCreditCard
-{
-    protected RTCreditCard()
-    {
+public class RTCreditCard {
+    protected RTCreditCard() {
 
     }
 
@@ -18,8 +16,7 @@ public class RTCreditCard
      * @param number credit card number as digits-only text string
      * @return <code>true</code> if card number passes the CRC check and belongs to one of the following credit card types: Visa, MasterCard, AmericanExpress, EnRoute, Diners, Maestro
      */
-    public static boolean isValidCreditCardNumber(String number)
-    {
+    public static boolean isValidCreditCardNumber(String number) {
         return getCreditCardType(number) != CreditCardType.UnknownOrInvalid && verifyCreditCardCRC(number);
     }
 
@@ -29,48 +26,37 @@ public class RTCreditCard
      * @param number credit card number as digits-only
      * @return <code>true</code> if CRC check passed
      */
-    public static boolean verifyCreditCardCRC(String number)
-    {
-        if (TextUtils.isEmpty(number))
-        {
+    public static boolean verifyCreditCardCRC(String number) {
+        if (TextUtils.isEmpty(number)) {
             return false;
         }
 
-        try
-        {
+        try {
             int j = number.length();
 
             String[] s1 = new String[j];
-            for (int i = 0; i < number.length(); i++)
-            {
+            for (int i = 0; i < number.length(); i++) {
                 s1[i] = "" + number.charAt(i);
             }
 
             int checksum = 0;
 
-            for (int i = s1.length - 1; i >= 0; i -= 2)
-            {
+            for (int i = s1.length - 1; i >= 0; i -= 2) {
                 int k = 0;
 
-                if (i > 0)
-                {
+                if (i > 0) {
                     k = Integer.valueOf(s1[i - 1]) * 2;
-                    if (k > 9)
-                    {
+                    if (k > 9) {
                         String s = "" + k;
                         k = Integer.valueOf(s.substring(0, 1)) + Integer.valueOf(s.substring(1));
                     }
                     checksum += Integer.valueOf(s1[i]) + k;
-                }
-                else
-                {
+                } else {
                     checksum += Integer.valueOf(s1[0]);
                 }
             }
             return ((checksum % 10) == 0);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
@@ -82,10 +68,8 @@ public class RTCreditCard
      * @param number number to check, only digits
      * @return credit card type this number belongs to or CreditCardType.UnknownOrInvalid for invalid or unknown numbers
      */
-    public static CreditCardType getCreditCardType(String number)
-    {
-        if (TextUtils.isEmpty(number))
-        {
+    public static CreditCardType getCreditCardType(String number) {
+        if (TextUtils.isEmpty(number)) {
             return CreditCardType.UnknownOrInvalid;
         }
 
@@ -94,52 +78,31 @@ public class RTCreditCard
         final String digit3 = number.length() > 2 ? number.substring(0, 3) : null;
         final String digit4 = number.length() > 3 ? number.substring(0, 4) : null;
 
-        if (TextUtils.isDigitsOnly(number.replaceAll(" ", "")))
-        {
-            if ("4".equals(digit1))
-            {
-                if (number.length() == 13 || number.length() == 16)
-                {
-                    return CreditCardType.Visa;
-                }
+        if ("4".equals(digit1)) {
+            if (number.length() == 13 || number.length() == 16) {
+                return CreditCardType.Visa;
             }
-            else if ("51".compareTo(digit2) < 0 && "55".compareTo(digit2) > 0)
-            {
-                if (number.length() == 16)
-                {
-                    return CreditCardType.MasterCard;
-                }
+        } else if ("51".compareTo(digit2) < 0 && "55".compareTo(digit2) > 0) {
+            if (number.length() == 16) {
+                return CreditCardType.MasterCard;
             }
-            else if ("34".equals(digit2) || "37".equals(digit2))
-            {
-                if (number.length() == 15)
-                {
-                    return CreditCardType.AmericanExpress;
-                }
+        } else if ("34".equals(digit2) || "37".equals(digit2)) {
+            if (number.length() == 15) {
+                return CreditCardType.AmericanExpress;
             }
-            else if ("2014".equals(digit4) || "2149".equals(digit4))
-            {
-                if (number.length() == 15)
-                {
-                    return CreditCardType.EnRoute;
-                }
+        } else if ("2014".equals(digit4) || "2149".equals(digit4)) {
+            if (number.length() == 15) {
+                return CreditCardType.EnRoute;
             }
-            else if ("36".equals(digit2) || "38".equals(digit2) || ("300".compareTo(digit3) < 0 && "305".compareTo(digit3) > 0))
-            {
-                if (number.length() == 14)
-                {
-                    return CreditCardType.Diners;
-                }
+        } else if ("36".equals(digit2) || "38".equals(digit2) || ("300".compareTo(digit3) < 0 && "305".compareTo(digit3) > 0)) {
+            if (number.length() == 14) {
+                return CreditCardType.Diners;
             }
-            else if ("5".equals(digit1) || "6".equals(digit1))
-            {
-                if (number.length() >= 12 && number.length() <= 19)
-                {
-                    return CreditCardType.Maestro;
-                }
+        } else if ("5".equals(digit1) || "6".equals(digit1)) {
+            if (number.length() >= 12 && number.length() <= 19) {
+                return CreditCardType.Maestro;
             }
         }
-
         return CreditCardType.UnknownOrInvalid;
     }
 
@@ -149,17 +112,14 @@ public class RTCreditCard
      * @param number digits-only string to format
      * @return formatted number
      */
-    public static String formatCreditCardNumberForDisplay(String number)
-    {
+    public static String formatCreditCardNumberForDisplay(String number) {
         final String num = number.replaceAll(" ", "");
         StringBuilder buf = new StringBuilder();
 
         //todo: add spaces according to credit card type
-        for (int i = 0; i < num.length(); i++)
-        {
+        for (int i = 0; i < num.length(); i++) {
             buf.append(num.charAt(i));
-            if (i == 3 || i == 7 || i == 11)
-            {
+            if (i == 3 || i == 7 || i == 11) {
                 buf.append(' ');
             }
         }
@@ -167,8 +127,7 @@ public class RTCreditCard
         return buf.toString();
     }
 
-    public enum CreditCardType
-    {
+    public enum CreditCardType {
         UnknownOrInvalid,
         Visa,
         MasterCard,
