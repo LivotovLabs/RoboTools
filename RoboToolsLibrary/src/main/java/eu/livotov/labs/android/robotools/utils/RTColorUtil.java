79b2c9a2fb28e38666ce0e064560d4281cfddeb6
grishko188cis <grishko@unrealmojo.com>
2016-11-10 16:07:22 +0200

    Update TimeUtil and RTColorUtil

--- RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/RTColorUtil.java
+++ RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/RTColorUtil.java
@@ -3,18 +3,20 @@ package eu.livotov.labs.android.robotools.utils;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.widget.ImageView;
 
 /**
  * Created by dlivotov on 22/10/2016.
  */
 
-public class RTColorUtil
-{
-    public static ColorStateList createColorStateList(Context ctx, @ColorRes int color)
-    {
+public class RTColorUtil {
+    public static ColorStateList createColorStateList(Context ctx, @ColorRes int color) {
         int[][] states = new int[][]{new int[]{android.R.attr.state_enabled}, // enabled
                 new int[]{-android.R.attr.state_enabled}, // disabled
                 new int[]{-android.R.attr.state_checked}, // unchecked
@@ -26,26 +28,31 @@ public class RTColorUtil
         return new ColorStateList(states, colors);
     }
 
-    public static void tintIcon(ImageView icon, @ColorRes int colorRes)
-    {
-        if (Build.VERSION.SDK_INT >= 21)
-        {
+    public static void tintIcon(ImageView icon, @ColorRes int colorRes) {
+        if (Build.VERSION.SDK_INT >= 21) {
             tintIcon21(icon, colorRes);
-        }
-        else
-        {
+        } else {
             tintIconCompat(icon, colorRes);
         }
     }
 
     @TargetApi(21)
-    private static void tintIcon21(ImageView icon, int colorRes)
-    {
+    private static void tintIcon21(ImageView icon, int colorRes) {
         icon.setImageTintList(ColorStateList.valueOf(icon.getContext().getResources().getColor(colorRes)));
     }
 
-    private static void tintIconCompat(ImageView icon, @ColorRes int colorRes)
-    {
+    private static void tintIconCompat(ImageView icon, @ColorRes int colorRes) {
         icon.setColorFilter(icon.getContext().getResources().getColor(colorRes));
+    }
+
+    public static Drawable tintDrawable(@DrawableRes int drawableRes, int color, Resources resources) {
+        Drawable original = resources.getDrawable(drawableRes);
+        if (original != null)
+            original.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        return original;
+    }
+
+    public static int addColorTransparency(int color) {
+        return (color & 0x00FFFFFF) | 0x40000000;
     }
 }
--- RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/TimeUtil.java
+++ RoboToolsLibrary/src/main/java/eu/livotov/labs/android/robotools/utils/TimeUtil.java
@@ -1,31 +1,43 @@
 package eu.livotov.labs.android.robotools.utils;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by dlivotov on 22/10/2016.
  */
 
-public class TimeUtil
-{
-    public static boolean isToday(long millis)
-    {
+public class TimeUtil {
+
+    private static final Calendar CALENDAR = Calendar.getInstance();
+
+    public static boolean isToday(long millis) {
         Calendar calendar = Calendar.getInstance();
         final int today = calendar.get(Calendar.DAY_OF_YEAR);
         calendar.setTimeInMillis(millis);
         return today == calendar.get(Calendar.DAY_OF_YEAR);
     }
 
-    public static boolean isTomorrow(long millis)
-    {
+    public static boolean isTomorrow(long millis) {
         Calendar calendar = Calendar.getInstance();
-        final int tomorrow = calendar.get(Calendar.DAY_OF_YEAR) + 1;
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        final int tomorrow = calendar.get(Calendar.DAY_OF_YEAR);
         calendar.setTimeInMillis(millis);
         return tomorrow == calendar.get(Calendar.DAY_OF_YEAR);
     }
 
-    public static long roundTimeToNearestNextMinutes(long millis, int round)
-    {
+    public static boolean isEqual(Date dateFirst, Date dateSecond) {
+        CALENDAR.setTime(dateFirst);
+        resetCalendarTime(CALENDAR);
+        dateFirst = CALENDAR.getTime();
+
+        CALENDAR.setTime(dateSecond);
+        resetCalendarTime(CALENDAR);
+        dateSecond = CALENDAR.getTime();
+        return dateFirst.equals(dateSecond);
+    }
+
+    public static long roundTimeToNearestNextMinutes(long millis, int round) {
         final Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(millis);
         int unroundedMinutes = calendar.get(Calendar.MINUTE);
@@ -37,8 +49,7 @@ public class TimeUtil
         return calendar.getTimeInMillis();
     }
 
-    public static long roundTimeToNearestMinutes(long millis, int round)
-    {
+    public static long roundTimeToNearestMinutes(long millis, int round) {
         final Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(millis);
         int unroundedMinutes = calendar.get(Calendar.MINUTE);
@@ -49,4 +60,46 @@ public class TimeUtil
 
         return calendar.getTimeInMillis();
     }
+
+    public static void resetCalendarTime(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    public static int nextMonth(int month) {
+        return month == Calendar.DECEMBER ? Calendar.JANUARY : month + 1;
+    }
+
+    public static int previousMonth(int month) {
+        return month == Calendar.JANUARY ? Calendar.DECEMBER : month - 1;
+    }
+
+    public static boolean isWeekendDay(int dayInWeek) {
+        return dayInWeek == Calendar.SUNDAY || dayInWeek == Calendar.SATURDAY;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        switch (month) {
+            case Calendar.JANUARY:
+            case Calendar.MARCH:
+            case Calendar.MAY:
+            case Calendar.JULY:
+            case Calendar.AUGUST:
+            case Calendar.OCTOBER:
+            case Calendar.DECEMBER:
+                return 31;
+            case Calendar.APRIL:
+            case Calendar.JUNE:
+            case Calendar.SEPTEMBER:
+            case Calendar.NOVEMBER:
+                return 30;
+            case Calendar.FEBRUARY:
+                return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? 29 : 28;
+            default:
+                throw new IllegalArgumentException("Invalid Month");
+        }
+    }
+
 }
